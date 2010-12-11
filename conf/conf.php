09c61d31eace48f3ca89385d4f7bc702f4d40131
mauguet.po <mauguet.po@gmail.com>
2010-12-11 14:03:18 +0000

    modif password mysql

--- conf/conf.php
+++ conf/conf.php
@@ -3,14 +3,14 @@
 
 $DB_HOST = 'localhost';
 $DB_LOGIN = 'root';
-$DB_PWD = 'mysql';
+$DB_PWD = 'garance';
 $DB_NAME = 'entreprises';
 
 //$URL_CONNEXION = "http://127.0.0.1/xampp/Mu/connexion.php";
 
 $PAGINATION = 5;
 
-//$DEBUG =true;
+$DEBUG =true;
 
 // E_ERROR | E_WARNING | E_PARSE | E_NOTICE | E_ALL
 //$LEVEL_ERR_PHP = E_PARSE;
